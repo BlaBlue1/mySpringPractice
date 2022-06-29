bba0bb98199f7052d86823e72309a9712a07e0dc
RuthT <87043497+BlaBlue1@users.noreply.github.com>
2022-06-29 19:08:22 -0400

    Add files via upload

---
+++ src/main/java/com/my_spring_practice/api_myapi/ApiMyapiApplication.java
@@ -0,0 +1,13 @@
+package com.my_spring_practice.api_myapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMyapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my_spring_practice/api_myapi/ApiMyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_spring_practice.api_myapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
